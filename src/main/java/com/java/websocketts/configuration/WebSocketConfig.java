440414bed8c881a95180278a31c9351ef559ad76
Deepak Chandh <deepak.chandh@tesco.com>
2024-05-07 22:00:31 +0530

    postman with websockets

--- src/main/java/com/java/websocketts/configuration/WebSocketConfig.java
+++ src/main/java/com/java/websocketts/configuration/WebSocketConfig.java
@@ -1,31 +1,25 @@
 package com.java.websocketts.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.messaging.simp.config.AbstractMessageBrokerConfiguration;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocket // We can enable WebSocket support in the Spring server
 // enabled a broker backed messaging over web socket using Stomp
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketConfigurer {
 
-    // in-memory message broker to carry the messages back to the client on destinations prefixed with “/topic”.
+
     @Override
-    public void configureMessageBroker(final MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler(), "/websocket");
     }
 
-    //registerStompEndpoints method registers the “/chat” endpoint, enabling Spring’s STOMP support.
-    @Override
-    public void registerStompEndpoints(final StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat");
-        registry.addEndpoint("/chat").withSockJS();
-//        registry.addEndpoint("/chatwithbots");
-//        registry.addEndpoint("/chatwithbots").withSockJS();
+    @Bean
+    public WebSocketHandler webSocketHandler() {
+        return new ServerWebSocketHandler();
     }
-
 }
